052412475cfed3c693cb322b89fc46d8f18d6e06
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2025-02-17 21:27:04 -0500

    git commit -m "Add images and videos to the public folder + media"

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,31 @@
 
+/* filepath: /Users/jazzyme/Downloads/Projects/Websites/Pristine-Electrical-Contractors/Pristine-Electricians-Website/pristine-electrical-contactors/public/js/script.js */
 
 $(document).ready(function(){
-    $('.project-gallery-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: true,
-        fade: true,
-        infinite: true,
-        speed: 500,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [
-            {
+  $('.project-gallery-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: true,
+      fade: true,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      cssEase: 'ease-in-out',
+      prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+      nextArrow: '<button type="button" class="slick-next">Next</button>',
+      responsive: [
+          {
               breakpoint: 768,
               settings: {
-                arrows: true,
-                dots: true
+                  arrows: true,
+                  dots: true,
+                  adaptiveHeight: true
               }
-            }
-        ]
-    });
+          }
+      ]
+  });
 });
 
 document.getElementById('contactForm').addEventListener('submit', async function (event) {
